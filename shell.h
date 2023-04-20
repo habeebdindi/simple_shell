3eb4f3a2d4c30da03b7f904f4d41d0b6e163c0bb
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-04-20 21:34:02 +0000

    Add header file

---
+++ shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+void print_prompt(char *prompt);
+void getinput(char **input, size_t *n);
+void forkcheck_fail(pid_t child, char *input);
+void fork_pass(char *input, char *av0);
+
+#endif /* MAIN_H */
