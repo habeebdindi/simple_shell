6812a37621b3e79e1c4cc0930f5b358af1347387
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-04-26 10:42:26 +0000

    Update execute_command's prototype

--- shell.h
+++ shell.h
@@ -6,15 +6,19 @@
 #include <string.h>
 #include <stdlib.h>
 #include <stddef.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <sys/stat.h>
 
 void print_prompt(char *prompt);
 void getinput(char **input, size_t *n);
 void forkcheck_fail(pid_t child, char *input);
 char **parser(char *input);
 void free_argv(char **argv);
-void execute_command(char **argv, char *av0, char *input);
+void execute_command(char **argv, char *av0, char *input, char *c);
+
+char *getpath(char *command);
 
 char *_strcpy(char *dest, char *src);
 int _strlen(char *s);
