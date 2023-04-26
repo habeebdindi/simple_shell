644097d62dc7ede8b2a47e0c8124d2279e962550
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-04-26 10:41:11 +0000

    Modulariz code, add to the execute_command function

--- shell_main.c
+++ shell_main.c
@@ -4,8 +4,6 @@ int main(__attribute__((unused)) int ac, char **av)
 {
         char *prompt = "#cisfun$ ", *c, *input = NULL, **argv;
         size_t n = 0;
-        pid_t child;
-        int status;
 
         if (isatty(0) == 0)
                 printf("%s", prompt);
@@ -15,29 +13,14 @@ int main(__attribute__((unused)) int ac, char **av)
 		getinput(&input, &n);
 		argv = parser(input);
 		if (!argv)
-		{
-			free(input);
 			continue;
-		}
 		c = getpath(argv[0]);
-		if (!(c))
+		if (!c)
 		{
 			free_argv(argv);
 			continue;
 		}
-		child = fork();
-		forkcheck_fail(child, input);
-		if (child == 0)
-		{
-			argv[0] = c;
-			execute_command(argv, av[0], input);
-		}
-		else
-		{
-                        wait(&status);
-			free(c);
-			free_argv(argv);
-		}
+		execute_command(argv, av[0], input, c);
         }
         return (0);
 }
