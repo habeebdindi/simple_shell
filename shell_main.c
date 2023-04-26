b996e2a609e84ea22b575f9162c33d314b6e1553
Habeeb Dindi <habeebullahdindi@gmail.com>
2023-04-26 19:57:05 +0000

    Correct error message

--- shell_main.c
+++ shell_main.c
@@ -1,14 +1,16 @@
 #include "shell.h"
+void sigint_handler(int signo);
 /**
  * main - main function for the shell.
  * @ac: argument count.
  * @av: ana array of pointers to the program.
  * Return: always 0 (succes).
  */
+
 int main(__attribute__((unused)) int ac, char **av)
 {
 	char *prompt = "#cisfun$ ", *c, error_message[200], *input = NULL, **argv;
-	size_t n = 0, message_length;
+	size_t n = 0, message_length, i = 0;
 
 	while (1)
 	{
@@ -21,7 +23,7 @@ int main(__attribute__((unused)) int ac, char **av)
 		if (!c)
 		{
 		        memset(error_message, 0, sizeof(error_message));
-			snprintf(error_message, sizeof(error_message), "%s: %d: %s: command not found\n", av[0], __LINE__, argv[0]);
+			snprintf(error_message, sizeof(error_message), "%s: %ld: %s: command not found\n", av[0], ++i, argv[0]);
 			message_length = strlen(error_message);
 			write(STDERR_FILENO, error_message, message_length);
 			free_argv(argv);
