e1b1076a315a4a9165778f4c018677f710db987c
gabrielkinyua <gabzkinyua@gmail.com>
2023-04-26 21:40:11 +0000

    Apply stashed changes

--- _exitt.c
+++ _exitt.c
@@ -10,7 +10,7 @@ void _exitt(char **argv, char *input)
 {
 	char *ptr = argv[0];
 
-	if (strcmp(ptr, "exit") == 0 && argv[1] == NULL)
+	if (_strcmp(ptr, "exit") == 0 && argv[1] == NULL)
 	{
 		free_argv(argv);
 		free(input);
